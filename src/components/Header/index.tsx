bbcd5f3fb591678c0543493e7ec4a3a3fb5a6d55
Priscilla <priscillaonil@gmail.com>
2023-11-07 10:36:08 -0300

    feat: header to mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
-// import styles from "./styles.module.scss";
+import styles from "./styles.module.scss";
 
+import { Logo } from "../../assets/svg/Logo";
+import { Phone } from "../../assets/svg/Phone";
 import { Button } from "../Button";
 
 export function Header() {
   return (
-    <div>
-      <Button icon="whatsapp" styleType="secondary">
-        Whatsapp
-      </Button>
+    <div className={styles.headerWrapper}>
+      <div className={styles.firstContent}>
+        <div className={styles.phone}>
+          <Phone size={19} color="#666666" />
+          <p>(21) 2042-4442</p>
+        </div>
+
+        <div>
+          <p>
+            <Logo size={35} />
+          </p>
+        </div>
+      </div>
+
+      <Button styleType="tertiary">Solicite uma cotação</Button>
     </div>
   );
 }
