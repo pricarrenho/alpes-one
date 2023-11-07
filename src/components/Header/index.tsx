25f701abe7002d6a76d68bfe99ef43a33df2f987
Priscilla <priscillaonil@gmail.com>
2023-11-07 10:41:12 -0300

    refactor: header to mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,25 +2,20 @@ import styles from "./styles.module.scss";
 
 import { Logo } from "../../assets/svg/Logo";
 import { Phone } from "../../assets/svg/Phone";
-import { Button } from "../Button";
 
 export function Header() {
   return (
     <div className={styles.headerWrapper}>
-      <div className={styles.firstContent}>
-        <div className={styles.phone}>
-          <Phone size={19} color="#666666" />
-          <p>(21) 2042-4442</p>
-        </div>
-
-        <div>
-          <p>
-            <Logo size={35} />
-          </p>
-        </div>
+      <div className={styles.phone}>
+        <Phone size={19} color="#666666" />
+        <p>(21) 2042-4442</p>
       </div>
 
-      <Button styleType="tertiary">Solicite uma cotação</Button>
+      <div>
+        <p>
+          <Logo size={35} />
+        </p>
+      </div>
     </div>
   );
 }
