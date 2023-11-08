d3cccea029934f8536ab91f62cad7de78aff7821
Priscilla <priscillaonil@gmail.com>
2023-11-08 11:47:05 -0300

    fix: type button

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,6 +1,10 @@
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
+
 export type ButtonProps = {
   children?: string | number;
   icon?: "phone" | "whatsapp" | "tour";
   styleType: "primary" | "secondary" | "tertiary" | "quaternary";
   as?: React.ElementType;
-};
+} & ButtonType;
