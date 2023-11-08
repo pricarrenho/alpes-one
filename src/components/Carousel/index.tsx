3fa6b95ed64618264e72e7da0b79e65cf201ecba
Priscilla <priscillaonil@gmail.com>
2023-11-08 11:50:03 -0300

    feat: placed responsive form in the carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import { Button } from "../Button";
-// import { Form } from "../Form";
+import { Form } from "../Form";
 import { Container } from "../Container";
 import styles from "./styles.module.scss";
 
 export function Carousel() {
+  const [formVisible, setFormVisible] = useState(false);
+
+  const handleOpenForm = () => {
+    setFormVisible(true);
+    document.querySelector("body")?.classList.add("no-scroll");
+  };
+
+  const handleCloseForm = () => {
+    setFormVisible(false);
+    document.querySelector("body")?.classList.remove("no-scroll");
+  };
+
   return (
     <div className={styles.carouselWrapper}>
-      <Button styleType="tertiary">Solicite uma cotação</Button>
+      <Button styleType="tertiary" onClick={handleOpenForm}>
+        Solicite uma cotação
+      </Button>
 
       <div className={styles.bannerBg}>
         <Container>
-          <div className={styles.carouselFormWrapper}>
+          <div className={styles.carouselFormWrapperMobile}>
+            {formVisible && (
+              <div>
+                <Form handleClose={handleCloseForm} />
+              </div>
+            )}
+          </div>
+
+          <div className={styles.carouselFormWrapperDesktop}>
             <div></div>
 
-            <div className={styles.carouselForm}>{/* <Form /> */}</div>
+            <div className={styles.carouselForm}>
+              <Form />
+            </div>
           </div>
         </Container>
 
