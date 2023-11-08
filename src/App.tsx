a030f6cc153f9b399d6f8476bb23d359d7d5d3c2
Priscilla <priscillaonil@gmail.com>
2023-11-08 10:06:33 -0300

    feat: created floating button for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Whatsapp } from "./assets/svg/Whatsapp";
 import { CardCars } from "./components/CardCars";
 import { Carousel } from "./components/Carousel";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import styles from "./styles.module.scss";
 
 export function App() {
   return (
@@ -13,6 +15,10 @@ export function App() {
       <CardCars />
 
       <Footer />
+
+      <button className={styles.whatsappButton}>
+        <Whatsapp size={36} color="#FFFFFF" />
+      </button>
     </div>
   );
 }
