489d33dc881585fbebd67534b36493ce7b389801
Priscilla <priscillaonil@gmail.com>
2023-11-08 10:05:56 -0300

    feat: create structure and style of header element in desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,61 @@ import styles from "./styles.module.scss";
 
 import { Logo } from "../../assets/svg/Logo";
 import { Phone } from "../../assets/svg/Phone";
+import { Button } from "../Button";
+import { Container } from "../Container";
 
 export function Header() {
   return (
-    <div className={styles.headerWrapper}>
-      <div className={styles.phone}>
-        <Phone size={19} color="#666666" />
-        <p>(21) 2042-4442</p>
-      </div>
+    <Container>
+      <header className={styles.headerWrapperMobile}>
+        <div className={styles.contactMobile}>
+          <Phone size={19} color="#666666" />
+          <p>(21) 2042-4442</p>
+        </div>
 
-      <div>
-        <p>
-          <Logo size={35} />
-        </p>
-      </div>
-    </div>
+        <div>
+          <p>
+            <Logo size={35} />
+          </p>
+        </div>
+      </header>
+
+      <header className={styles.headerWrapperDesktop}>
+        <div className={styles.contactDesktop}>
+          <div className={styles.buttonsWrapper}>
+            <Button styleType="primary" icon="phone">
+              (21) 2042-4442
+            </Button>
+            <Button styleType="primary" icon="tour">
+              Tour Virtual
+            </Button>
+            <Button styleType="secondary" icon="whatsapp">
+              Whatsapp
+            </Button>
+          </div>
+
+          <div className={styles.ruler}>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>7</p>
+            <p>8</p>
+            <p>X</p>
+            <p>Z</p>
+            <p>M</p>
+            <p>i</p>
+            <p>Plug-in-Hybrid</p>
+          </div>
+        </div>
+
+        <div>
+          <p>
+            <Logo size={92} />
+          </p>
+        </div>
+      </header>
+    </Container>
   );
 }
