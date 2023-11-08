30863a230a943f573c8bc86ba8443bbc6a58955a
Priscilla <priscillaonil@gmail.com>
2023-11-07 22:43:48 -0300

    feat: create structure and style of carousel element in desktop

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,14 +1,28 @@
-import styles from "./styles.module.scss";
-import mobileBanner from "../../assets/img/mobile-banner.png";
-
 import { Button } from "../Button";
+// import { Form } from "../Form";
+import { Container } from "../Container";
+import styles from "./styles.module.scss";
 
 export function Carousel() {
   return (
-    <div className={styles.banner}>
+    <div className={styles.carouselWrapper}>
       <Button styleType="tertiary">Solicite uma cotação</Button>
 
-      <img src={mobileBanner} />
+      <div className={styles.bannerBg}>
+        <Container>
+          <div className={styles.carouselFormWrapper}>
+            <div></div>
+
+            <div className={styles.carouselForm}>{/* <Form /> */}</div>
+          </div>
+        </Container>
+
+        <div className={styles.dots}>
+          <div className={styles.firstDot}></div>
+          <div className={styles.dot}></div>
+          <div className={styles.dot}></div>
+        </div>
+      </div>
     </div>
   );
 }
